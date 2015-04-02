a47a76949afc4f5735d1b695c8810ccbbb17fa09
Vivek Ragunathan <vivekragunathan@gmail.com>
2015-04-01 21:31:05 -0700

    Modeling updates

--- AndroidApp/TheFoodApp/app/src/main/java/com/projectspeedracer/thefoodapp/TheFoodApplication.java
+++ AndroidApp/TheFoodApp/app/src/main/java/com/projectspeedracer/thefoodapp/TheFoodApplication.java
@@ -23,9 +23,9 @@ public class TheFoodApplication extends Application {
 
 	public static final String GOOGLE_API_KEY = "AIzaSyD6UJCC4Ey_VdaWqVB-AVEdur7_yu-cAyM"; // server key - works
 
-	private static final float DEFAULT_SEARCH_DISTANCE = 500.0f * 1000; // in feet
+	public static final float DEFAULT_SEARCH_DISTANCE = 500.0f * 100000; // in feet
 
-	public static final Boolean isLocal = false; // true for testing
+	public static final Boolean isLocal = true; // true for testing
 
 	public static final int MAX_NUM_PLACES = 4;
 
