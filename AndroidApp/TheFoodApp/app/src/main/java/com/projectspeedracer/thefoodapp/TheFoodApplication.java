4b550c3b57bd6a6e67b6015c60ae39e78370afd0
Avaneesh Kadam <avaneesh.kadam@gmail.com>
2015-04-08 16:50:24 -0700

    Demo recording and going back to live settings!!

--- AndroidApp/TheFoodApp/app/src/main/java/com/projectspeedracer/thefoodapp/TheFoodApplication.java
+++ AndroidApp/TheFoodApp/app/src/main/java/com/projectspeedracer/thefoodapp/TheFoodApplication.java
@@ -27,9 +27,9 @@ public class TheFoodApplication extends Application {
 
 	public static final String GOOGLE_API_KEY = "AIzaSyD6UJCC4Ey_VdaWqVB-AVEdur7_yu-cAyM"; // server key - works
 
-	public static final float DEFAULT_SEARCH_DISTANCE = 500.0f * 10000; // in feet
+	public static final float DEFAULT_SEARCH_DISTANCE = 500.0f; // in feet
 
-	public static final Boolean isLocal = true; // true for testing
+	public static final Boolean isLocal = false; // true for testing
 
 	public static final int MAX_NUM_PLACES = 6;
 
